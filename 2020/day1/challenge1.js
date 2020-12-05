29b4e4226bd0809fd2fa6bdcf11770c2759ad54b
Michael <Michael@MichaelHumiston.com>
2020-12-05 09:24:55 -0500

    Day 1: Challenge 2

--- 2020/day1/challenge1.js
+++ 2020/day1/challenge1.js
@@ -5,8 +5,17 @@ const input = fs.readFileSync('./challenge1Input.txt', 'utf8').split('\n').map(x
 for (let x = 0; x < input.length - 1; x++) {
   for (let y = x + 1; y < input.length; y++) {
     if (input[x] + input[y] == 2020) {
-      console.log(input[x] * input[y])
+      console.log("Challenge 1: " + input[x] * input[y])
     }
   }
 }
 
+for (let x = 0; x < input.length - 2; x++) {
+  for (let y = x + 1; y < input.length; y++) {
+    for (let z = y + 1; z < input.length; z++) {
+      if (input[x] + input[y] + input[z] == 2020) {
+        console.log("Challenge 1: " + input[x] * input[y] * input[z])
+      }
+    }
+  }
+}
